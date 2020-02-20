b882b3dcfd424f875d55c10764c7d2b984c2f2d7
Bernardo Ramos <up201505092@fe.up.pt>
2020-02-20 18:40:05 +0000

    types ass int and orde

--- practice-pizza-problem/python/pizzaGreedy.py
+++ practice-pizza-problem/python/pizzaGreedy.py
@@ -1,29 +1,35 @@
-import copy
-import itertools
+from operator import itemgetter
 
 hashGlobal = dict()
 
 
 def main():
-    infile = open("a_example.txt", "r")
+    infile = open("b_read_on.txt", "r")
     firstLine = infile.readline().split()
-    numBooks = firstLine[0]
-    numLibs = firstLine[1]
-    numDays = firstLine[2]
+    numBooks = int(firstLine[0])
+    numLibs = int(firstLine[1])
+    numDays = int(firstLine[2])
     scores = infile.readline().split()
+    scores = [int(i) for i in scores]
     allLibraries = []
     for i in range(int(numLibs)):
         library = dict()
         firstLine = infile.readline().split()
-        library['numBooks'] = firstLine[0]
-        library['signDays'] = firstLine[1]
-        library['booksPerDay'] = firstLine[2]
+        library['numBooks'] = int(firstLine[0])
+        library['signDays'] = int(firstLine[1])
+        library['booksPerDay'] = int(firstLine[2])
         secondLine = infile.readline().split()
         library['listBooks'] = secondLine
         allLibraries.append(library)
-    print(allLibraries)
+    orderingSignUp(allLibraries)
 
-# def auxCombination(): # Calculates sum for each combo
+
+def orderingSignUp(allLibraries):
+    #sorting by longest signup period
+    sortedLibraries = sorted(allLibraries, key=itemgetter('signDays'))
+    for i in sortedLibraries:
+        print(i)
+
 
 if __name__ == '__main__':
     main()
